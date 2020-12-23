3d1da330594146669000b3401000954cfd01c498
Frodo Baggins <jm@marinho.com.es>
2020-12-23 22:05:55 +0100

    New Graph class

--- ghreghorio.ino
+++ ghreghorio.ino
@@ -1,51 +1,35 @@
-#include <TFT_eSPI.h>
-#include <SPI.h>
-#include "WiFi.h"
-#include <Wire.h>
-#include <Button2.h>
-#include "esp_adc_cal.h"
-#include "images/logo.h"
+//FOR TTGO_T_Display
+// Select /User_Setups/Setup25_TTGO_T_Display.h in TFT_eSPI/User_Setup_Select.h
+//FOR TTGO_T4
+// Select <User_Setups/Setup22_TTGO_T4_v1.3.h> in TFT_eSPI/User_Setup_Select.h
 
-#ifndef TFT_DISPOFF
-#define TFT_DISPOFF 0x28
-#endif
+#define GHREGHORIO_FOR_T4
+#include "ghreghorio.h"
 
-#ifndef TFT_SLPIN
-#define TFT_SLPIN   0x10
-#endif
+long currentMillis;
+long previousMillis;
+unsigned long currentMicros;
+unsigned long previousMicros;
 
-#define TFT_MOSI            19
-#define TFT_SCLK            18
-#define TFT_CS              5
-#define TFT_DC              16
-#define TFT_RST             23
+unsigned long curCount;       //number of counts in last measuring interval
+unsigned long avgCount;       //average number of counts in last n intervals
+unsigned long totCount;       //total number of counts
+float curDoseRate;            //current dose rate in last measuring interval
+float avgDoseRate;            //average dose rate in last n intervals
+float totDoseRate;            //accumulated total dose
 
-#define TFT_BL          4  // Display backlight control pin
-#define ADC_EN          14
-#define ADC_PIN         34
-#define BUTTON_1        35
-#define BUTTON_2        0
-
-TFT_eSPI tft = TFT_eSPI(135, 240); // Invoke custom library
+TFT_eSPI tft = TFT_eSPI(); // Invoke custom library
 Button2 btn1(BUTTON_1);
 Button2 btn2(BUTTON_2);
+
+TFTGraph ShortTermGraph(&tft, STZ_LEFT, STZ_TOP, STZ_WIDTH, STZ_HEIGHT, STZ_AXC, STZ_AXC, STZ_BGC, STZ_SVC);
+TFTGraph LongTermGraph(&tft, LTZ_LEFT, LTZ_TOP, LTZ_WIDTH, LTZ_HEIGHT, LTZ_AXC, LTZ_AXC, LTZ_BGC, LTZ_SVC);
 
 char buff[512];
 int vref = 1100;
 int btnCick = false;
 
-
-//  tft.fillRect(0, 0, M_SIZE*239, M_SIZE*131, TFT_GREY);
-//  tft.setTextColor(TFT_BLACK);  // Text colour
-//  tft.fillTriangle(x0, y0, x1, y1, x2, y2, TFT_GREEN);
-//  tft.drawLine(x0, y0, x1, y1, TFT_BLACK);
-//  tft.drawCentreString("0", x0+4, y0-4, 1);
-//  tft.drawString("%RH", M_SIZE*(3 + 230 - 40), M_SIZE*(119 - 20), 2); // Units at bottom right
-//  tft.drawRect(1, M_SIZE*3, M_SIZE*236, M_SIZE*126, TFT_BLACK); // Draw bezel line
-//  tft.drawRightString(buf, 33, M_SIZE*(119 - 20), 2);
-
-
-
+int start = 0; //solo para hacer que las gráficas se muevan
 
 //! Long time delay, it is recommended to use shallow sleep, which can effectively reduce the current consumption
 void espDelay(int ms)
@@ -55,73 +39,48 @@ void espDelay(int ms)
     esp_light_sleep_start();
 }
 
-void showVoltage()
-{
-    static uint64_t timeStamp = 0;
-    if (millis() - timeStamp > 1000) {
-        timeStamp = millis();
-        uint16_t v = analogRead(ADC_PIN);
-        float battery_voltage = ((float)v / 4095.0) * 2.0 * 3.3 * (vref / 1000.0);
-        String voltage = "Voltage :" + String(battery_voltage) + "V";
-        Serial.println(voltage);
-        tft.fillScreen(TFT_BLACK);
-        tft.setTextDatum(MC_DATUM);
-        tft.drawString(voltage,  tft.width() / 2, tft.height() / 2 );
-    }
-}
+
 
 void setup()
 {
-    Serial.begin(115200);
-    Serial.println("Start");
+  Serial.begin(115200);
+  Serial.println("Start");
 
-    esp_adc_cal_characteristics_t adc_chars;
-    esp_adc_cal_value_t val_type = esp_adc_cal_characterize((adc_unit_t)ADC_UNIT_1, (adc_atten_t)ADC1_CHANNEL_6, (adc_bits_width_t)ADC_WIDTH_BIT_12, 1100, &adc_chars);
-    //Check type of calibration value used to characterize ADC
-    if (val_type == ESP_ADC_CAL_VAL_EFUSE_VREF) {
-        Serial.printf("eFuse Vref:%u mV", adc_chars.vref);
-        vref = adc_chars.vref;
-    } else if (val_type == ESP_ADC_CAL_VAL_EFUSE_TP) {
-        Serial.printf("Two Point --> coeff_a:%umV coeff_b:%umV\n", adc_chars.coeff_a, adc_chars.coeff_b);
-    } else {
-        Serial.println("Default Vref: 1100mV");
+  esp_adc_cal_characteristics_t adc_chars;
+  esp_adc_cal_value_t val_type = esp_adc_cal_characterize((adc_unit_t)ADC_UNIT_1, (adc_atten_t)ADC1_CHANNEL_6, (adc_bits_width_t)ADC_WIDTH_BIT_12, 1100, &adc_chars);
+  //Check type of calibration value used to characterize ADC
+  if (val_type == ESP_ADC_CAL_VAL_EFUSE_VREF)
+    {
+    Serial.printf("eFuse Vref:%u mV", adc_chars.vref);
+    vref = adc_chars.vref;
+    }
+  else if (val_type == ESP_ADC_CAL_VAL_EFUSE_TP)
+    {
+    Serial.printf("Two Point --> coeff_a:%umV coeff_b:%umV\n", adc_chars.coeff_a, adc_chars.coeff_b);
+    }
+  else
+    {
+    Serial.println("Default Vref: 1100mV");
     }
 
-    
-    tft.init();
-    tft.setRotation(1);
-    tft.fillScreen(TFT_BLACK);
-    tft.setTextSize(2);
-    tft.setTextColor(TFT_WHITE);
-    tft.setCursor(0, 0);
-    tft.setTextDatum(MC_DATUM);
-    tft.setTextSize(2);
+  tft.begin();
+  tft.setRotation(1);
+  tft.setSwapBytes(true);
 
-    if (TFT_BL > 0) { // TFT_BL has been set in the TFT_eSPI library in the User Setup file TTGO_T_Display.h
-         pinMode(TFT_BL, OUTPUT); // Set backlight pin to output mode
-         digitalWrite(TFT_BL, TFT_BACKLIGHT_ON); // Turn backlight on. TFT_BACKLIGHT_ON has been set in the TFT_eSPI library in the User Setup file TTGO_T_Display.h
+  if (TFT_BL > 0) // TFT_BL has been set in the TFT_eSPI library in the User Setup file TTGO_T_Display.h
+    {
+    pinMode(TFT_BL, OUTPUT); // Set backlight pin to output mode
+    digitalWrite(TFT_BL, TFT_BACKLIGHT_ON); // Turn backlight on. TFT_BACKLIGHT_ON has been set in the TFT_eSPI library in the User Setup file TTGO_T_Display.h
     }
 
-    tft.setSwapBytes(true);
-    tft.pushImage(0, 0,  240, 135, logo);
-    espDelay(10000);
-    tft.fillScreen(TFT_BLACK);
-    tft.drawString("This is a homemade",  tft.width() / 2, tft.height() / 2 );
-    espDelay(2000);
-    tft.fillScreen(TFT_BLACK);
-    tft.drawString("Geiger counter.",  tft.width() / 2, tft.height() / 2 );
-    espDelay(2000);
-    tft.fillScreen(TFT_BLACK);
-    tft.drawString("USE IT",  tft.width() / 2, tft.height() / 2 );
-    espDelay(2000);
-    tft.fillScreen(TFT_BLACK);
-    tft.drawString("AT YOUR OWN",  tft.width() / 2, tft.height() / 2 );
-    espDelay(2000);
-    tft.setTextSize(3);
-    tft.fillScreen(TFT_BLACK);
-    tft.drawString("RISK",  tft.width() / 2, tft.height() / 2 );
-    espDelay(2000);
-    tft.setTextSize(2);
+  //drawIntro()
+
+  ShortTermGraph.Init();
+  ShortTermGraph.ClearGraph();
+  ShortTermGraph.DrawAxis();
+  LongTermGraph.Init();
+  LongTermGraph.ClearGraph();
+  LongTermGraph.DrawAxis();
 
 }
 
@@ -129,9 +88,119 @@ void setup()
 
 void loop()
 {
-    showVoltage();
+drawCurrentData();
+
+#ifdef DEBUG
+Serial.println("Drawing Short-term Graph:\n");
+Serial.printf("STZ_LEFT = %d\n", STZ_LEFT);
+Serial.printf("STZ_TOP = %d\n", STZ_TOP);
+Serial.printf("STZ_WIDTH = %d\n", STZ_WIDTH);
+Serial.printf("STZ_HEIGHT = %d\n", STZ_HEIGHT);
+Serial.printf("STZ_XYOR_X = %d\n", STZ_XYOR_X);
+Serial.printf("STZ_XYOR_Y = %d\n", STZ_XYOR_Y);
+Serial.printf("STZ_XAXIS_END = %d\n", STZ_XAXIS_END);
+Serial.printf("STZ_YAXIS_END = %d\n", STZ_YAXIS_END);
+Serial.printf("STZ_XAXIS_L = %d\n", STZ_XAXIS_L);
+Serial.printf("STZ_YAXIS_L = %d\n", STZ_YAXIS_L);
+#endif
+
+
+for (int i = 0; i < 150; i++)
+  {
+    ShortTermGraph.SetValue(i, int(80*(1+sin(2*PI*(i+start)/100))));
+  }
+
+//ShortTermGraph.ClearValues(); //Si borramos la zona se nota parpadeo
+ShortTermGraph.DrawValues();
+
+
+
+#ifdef DEBUG
+Serial.println("Drawing Long-term Graph:\n");
+Serial.printf("LTZ_LEFT = %d\n", LTZ_LEFT);
+Serial.printf("LTZ_TOP = %d\n", LTZ_TOP);
+Serial.printf("LTZ_WIDTH = %d\n", LTZ_WIDTH);
+Serial.printf("LTZ_HEIGHT = %d\n", LTZ_HEIGHT);
+Serial.printf("LTZ_XYOR_X = %d\n", LTZ_XYOR_X);
+Serial.printf("LTZ_XYOR_Y = %d\n", LTZ_XYOR_Y);
+Serial.printf("LTZ_XAXIS_END = %d\n", LTZ_XAXIS_END);
+Serial.printf("LTZ_YAXIS_END = %d\n", LTZ_YAXIS_END);
+Serial.printf("LTZ_XAXIS_L = %d\n", LTZ_XAXIS_L);
+Serial.printf("LTZ_YAXIS_L = %d\n", LTZ_YAXIS_L);
+#endif
+
+
+for (int i = 0; i < 300; i++)
+  {
+    LongTermGraph.SetValue(i, int(25*(1+sin(2*PI*(i+start)/100))));
+  }
+
+//LongTermGraph.ClearValues(); //Si borramos la zona se nota parpadeo
+LongTermGraph.DrawValues();
+
+
+
+delay(10);
+start += 1;
 }
 
 
+void drawCurrentData()
+{
+  #ifdef DEBUG
+  Serial.println("Drawing Current Data:\n");
+  Serial.printf("CDZ_LEFT = %d\n", CDZ_LEFT);
+  Serial.printf("CDZ_TOP = %d\n", CDZ_TOP);
+  Serial.printf("CDZ_WIDTH = %d\n", CDZ_WIDTH);
+  Serial.printf("CDZ_HEIGHT = %d\n", CDZ_HEIGHT);
+  #endif
 
-  
+  tft.setTextSize(1);
+  tft.setTextColor(CDZ_TXC);
+  tft.fillRect(CDZ_LEFT, CDZ_TOP, CDZ_WIDTH, CDZ_HEIGHT, CDZ_BGC);
+  tft.setCursor(CDZ_LEFT+1, CDZ_TOP+1);
+  tft.print("GHREGHORIO");
+
+  String batteryCap = "Batt:" + String( readBatteryCap() ) + "%";
+
+  tft.setCursor(CDZ_RIGHT-1, CDZ_TOP+1);
+  tft.print(batteryCap);
+}
+
+int readBatteryCap()
+{
+  return 100;
+}
+
+void drawIntro()
+{
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextColor(TFT_WHITE);
+  tft.setCursor(0, 0);
+  tft.setTextDatum(MC_DATUM);
+  tft.setTextSize(2);
+  tft.pushImage(0, 0,  MAXX, MAXY, LOGO);
+  espDelay(5000);
+
+  tft.fillScreen(TFT_BLACK);
+  tft.drawString("This is a homemade",  tft.width() / 2, tft.height() / 2 );
+  espDelay(2000);
+
+  tft.fillScreen(TFT_BLACK);
+  tft.drawString("Geiger counter.",  tft.width() / 2, tft.height() / 2 );
+  espDelay(2000);
+
+  tft.fillScreen(TFT_BLACK);
+  tft.drawString("USE IT",  tft.width() / 2, tft.height() / 2 );
+  espDelay(2000);
+
+  tft.fillScreen(TFT_BLACK);
+  tft.drawString("AT YOUR OWN",  tft.width() / 2, tft.height() / 2 );
+  espDelay(2000);
+
+  tft.setTextSize(3);
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextColor(TFT_RED);
+  tft.drawString("RISK",  tft.width() / 2, tft.height() / 2 );
+  espDelay(2000);
+}
